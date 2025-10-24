559b2c6e843cf6f18f9e9eb98705e4a6bd441c37
pacse <packard.seth@gmail.com>
2025-10-24 21:01:23 +1300

    removed unnecessary ;'s, added error handling

--- aliases.sh
+++ aliases.sh
@@ -1,51 +1,81 @@
 #!/usr/bin/env bash
 
-# simple stuff aliases
-alias cls="clear"; # tehehe
-alias shutdown="sudo shutdown -h now";
-alias restart="sudo shutdown -r now";
+# shortenings
+SPOTIFY_APP="com.spotify.Client"
 
-alias ll='ls -la --color=auto';
-alias la='ls -A --color=auto';
+
+# simple stuff aliases
+alias cls="clear" # tehehe
+alias sd="echo 'Shutting down . . .'; sudo shutdown -h now"
+alias rs="echo 'Restarting . . .'; sudo shutdown -r now"
+
+alias ll='ls -la --color=auto'
+alias la='ls -A --color=auto'
 
 
 # nix aliases
 rebuild() {
-    sudo nixos-rebuild switch --flake /home/evren/flake#evren \
-        && rm /home/evren/.cache/tofi-drun;
+    if sudo nixos-rebuild switch --flake $HOME/flake#evren; then
+        rm -f $HOME/.cache/tofi-drun
+        echo "Rebuild successful :)"
+    else
+        echo "Rebuild failed!"
+        return 1
+    fi
 }
 
-alias tidy="nix-collect-garbage --delete-older-than 7d";
-alias update="nix flake update && rebuild";
+alias tidy="nix-collect-garbage --delete-older-than 7d"
+alias up="nix flake update && rebuild"
+alias rb="rebuild"
 
 
 # flatpak
-alias flatup="flatpak update -y";
-alias spotify="flatpak run com.spotify.Client";
-
 flatpak_reinstall() {
     flatpak remote-add --if-not-exists flathub \
-        https://flathub.org/repo/flathub.flatpakrepo;
+        https://flathub.org/repo/flathub.flatpakrepo
 
     # packages, hopefully won't end up with more here
-    flatpak install flathub com.spotify.Client -y;
+    flatpak install flathub $SPOTIFY_APP -y
 }
 
+alias flatup="flatpak update -y"
+alias flatre="flatpak_reinstall"
+alias spotify="flatpak run $SPOTIFY_APP"
+
+
 # git
-alias gs='git status';
-alias gpl='git pull';
-alias gps='git push';
+alias gs='git status'
+alias gpl='git pull'
+alias gps='git push'
+alias grs='git restore --staged .'
 
 gc() {
-    git commit -m "$1";
+    if [ -z "$1" ]; then
+        echo "Please provide a commit message."
+        return 1
+
+    else
+        git commit -m "$1"
+        git push
+    fi
 }
 
 gd() {
-if [ -z "$1" ]; then
-    git diff;
+    if [ -z "$1" ]; then
+        git diff
 
-else
-    git diff "$1";
+    else
+        git diff "$1"
 
-fi
+    fi
+}
+
+ga() {
+    if [ -z "$1" ]; then
+        git add .
+
+    else
+        git add "$@"
+
+    fi
 }
